d3f90a885b93cc09f3419d0c50b63a600ec6897c
Blessings Moyo <blessisngsmoyo222@gmail.com>
2024-12-17 17:33:50 -0600

    created stopper timer

---
+++ script.js
@@ -0,0 +1,23 @@
+function updateCountdown() {
+  const now = new Date();
+  const eventDate = new Date('Dec 17 2024, 18:00:00');
+  const currentTime = now.getTime();
+  let remainingTime = eventDate.getTime() - currentTime;
+
+  if (remainingTime < 0) {
+      remainingTime = 0;
+  }
+
+  const seconds = Math.floor((remainingTime / 1000) % 60);
+  const minutes = Math.floor((remainingTime / 1000 / 60) % 60);
+  const hours = Math.floor((remainingTime / 1000 / 60 / 60) % 24);
+  const days = Math.floor(remainingTime / 1000 / 60 / 60 / 24);
+
+  document.getElementById('days').textContent = String(days).padStart(2, '0');
+  document.getElementById('hours').textContent = String(hours).padStart(2, '0');
+  document.getElementById('minutes').textContent = String(minutes).padStart(2, '0');
+  document.getElementById('seconds').textContent = String(seconds).padStart(2, '0');
+  
+}
+
+setInterval(updateCountdown, 1000);
